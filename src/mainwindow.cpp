1a158375f84e4b974212d80f2f7e37c4b4ae1fce
el-famos0 <37973912+el-famos0@users.noreply.github.com>
2018-05-31 19:03:07 +1000

    pipi

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -189,6 +189,8 @@ QMessageBox::Yes, QMessageBox::No);
 
     configInstance->setModal(true);
 
+    run.done = true;
+
     connect(configInstance, SIGNAL(rejected()), this, SLOT(close()));
     connect(configInstance, SIGNAL(accepted()), this, SLOT(connectionTest()));
 
@@ -237,6 +239,7 @@ void MainWindow::connectionTest()
 
     command = commandString.generateCommand();
     settings.setValue(REGISTRY_KEY_COMMAND, command);
+    run.done = false;
 
 }
 
