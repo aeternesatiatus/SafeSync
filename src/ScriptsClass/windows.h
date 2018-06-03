00e31e0addc207fb0aa8850c4d9a30a45be7a166
el-famos0 <el-famos0>
2018-06-03 14:00:24 +1000

    REMOVE ECHO OFF

--- src/ScriptsClass/windows.h
+++ src/ScriptsClass/windows.h
@@ -5,7 +5,7 @@
 
 namespace windows
 {
-    static const QString first = "@echo off & date /T >> rsync.log & time /T >> rsync.log & rsync -e 'ssh -i C:\\Users\\";
+    static const QString first = "date /T >> rsync.log & time /T >> rsync.log & rsync -e 'ssh -i C:\\Users\\";
     // second clientUser
     static const QString third = "\\.ssh\\id_rsa' -av --delete --hard-links --progress ' //localhost";
     // fourth: clientDir
